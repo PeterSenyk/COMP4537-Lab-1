beec55d08313401de2410cdd1068e6a2995a572b
Peter <Peter.Senyk@gmail.com>
2025-09-16 10:55:54 -0700

    chore: update script.js code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const elements = document.querySelectorAll('*');
     elements.forEach(element => {
-        if (element.children.length === 0) { // Only process leaf nodes
+        if (element.children.length === 0) { 
             element.innerHTML = element.innerHTML.replace(/\$\{MESSAGES\.(\w+)\}/g, (match, key) => {
                 return MESSAGES[key] || match;
             });
